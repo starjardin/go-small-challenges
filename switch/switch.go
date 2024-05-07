01c57f3924682fb02ca4e7a4cc241fff03b33716
starjardin <tantely.and@onja.org>
2024-05-07 11:55:01 +0300

    [core] Learning switch

---
+++ switch/switch.go
@@ -0,0 +1,66 @@
+// Hya, YES I did not forget, just happened to be busy
+// Today we are going to look at switch statements: Switch statements express conditionals across many branches.
+// I think this one is not difficult to grasp, so we will go straight into examples
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	// Here’s a basic switch.
+
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	// You can use commas to separate multiple expressions in the same case statement.
+	// We use the optional default case in this example as well.
+	// We check if the weekday is Saturday or Sunday in one case
+	// Default case is Mon - Fri
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	// switch without an expression is an alternate way to express if/else logic.
+	// Here we also show how the case expressions can be non-constants.
+	// see that we don't have anything after the switch, this is also possible
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	// A type switch compares types instead of values.
+	// You can use this to discover the type of an interface value.
+	// In this example, the variable t will have the type corresponding to its clause.
+	whatAmI := func(i interface{}) {
+		// is this not something new to us?
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
